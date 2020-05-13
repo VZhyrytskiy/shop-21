8136e7bf68adf857daa92aba7ace266c48b189f1
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-05-13 16:25:12 +0300

    task1-comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { CartComponent } from './cart/components/cart.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-// проект не запускается, веротно не все выложено
+// проект не запускается, вероятно не все выложено
 // ERROR in src/app/app.module.ts(8,38): error TS2307: Cannot find module './products/components/product-list/product-list.component'.
 // src/app/app.module.ts(9,31): error TS2307: Cannot find module './cart/components/cart.component'.
 // src/app/products/components/product/index.ts(1,34): error TS2307: Cannot find module './product.component'.
